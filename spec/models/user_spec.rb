b7f1856408717669676a50b93bd6faf817189c8a
learn <learn@learn10.local>
2016-09-27 11:58:53 -0700

    added devise and user login

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  it 'is a thing' do
+    expect {User.new}.to_not raise_error
+  end
+
+  it 'has an email and password' do
+    user = User.new
+    user.email = 'bob@bob.com'
+    user.password = 'bobbob'
+    expect(user.save).to be true
+    user2 = User.find_by_email('bob@bob.com')
+    expect(user2.email).to eq 'bob@bob.com'
+  end
+
+end
