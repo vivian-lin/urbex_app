7a3d279606a55d88ada937b514fcaf6a657b36f3
learn <learn@learn10.local>
2016-09-27 14:04:29 -0700

    added user logout link to navbar

--- spec/features/landing_pages_spec.rb
+++ spec/features/landing_pages_spec.rb
@@ -15,10 +15,11 @@ RSpec.feature "LandingPages", type: :feature do
 
   context 'Going to an about page' do
     Steps 'I can click the nav-bar link called About' do
-      Given 'I am on the landing page' do
+      Given 'I am on the landing page, and I can see a link in the nav bar called About' do
         visit '/'
+        expect(page).to have_content "About"
       end # ends given
-      Then 'I am taken to the about page' do
+      Then 'I can click the link and am taken to the About page' do
         click_link('About')
         expect(page).to have_content "About UrbEx"
       end # ends then
--- spec/features/users_spec.rb
+++ spec/features/users_spec.rb
@@ -6,12 +6,28 @@ RSpec.feature "Users", type: :feature do
       Given 'I am on the sign up page' do
         visit '/users/sign_up'
       end
-      Then 'I can enter my email an password' do
+      Then 'I can enter my email and password' do
         fill_in 'user[email]', with: 'joe@joe.com'
         fill_in 'user[password]', with: 'joejoe'
         fill_in 'user[password_confirmation]', with: 'joejoe'
         click_button 'Sign up'
-        expect(page).to have_content("UrbEx")
+        expect(page).to have_content("Welcome! You have signed up successfully.")
+      end
+    end
+  end
+
+  context 'Logging out of an account' do
+    Steps 'Go to sign up page' do
+      Given 'I am on the sign up page' do
+        visit '/users/sign_up'
+      end
+      Then 'I can enter my email and password' do
+        fill_in 'user[email]', with: 'joe@joe.com'
+        fill_in 'user[password]', with: 'joejoe'
+        fill_in 'user[password_confirmation]', with: 'joejoe'
+        click_button 'Sign up'
+        click_link 'Sign out'
+        expect(page).to have_content('Signed out successfully')
       end
     end
   end
