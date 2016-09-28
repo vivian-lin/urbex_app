e38605bc51618128c95b0484401b50ddcc6cdd15
learn <learn@learn06.local>
2016-09-28 16:30:52 -0700

    WIP added papper clip can add image to user not tested

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
+  has_attached_file :image, styles: { large: "600x600>", medium: "300x300>", thumb: "150x150#"}
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable, :omniauthable,
