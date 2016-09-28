0595bb04bba1e33ad64fc0e7887635ae83488232
learn <learn@learn06.local>
2016-09-28 10:30:34 -0700

    Added and tested Sign in and Sign up in navabar and changed About page into a static html dot erb file

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  get 'about/index'
+
   devise_for :users, controllers: {omniauth_callbacks: "omniauth_callbacks"}
 
   # The priority is based upon order of creation: first created -> highest priority.
