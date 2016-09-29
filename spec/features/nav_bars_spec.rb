196bee0e72a8b2b7eff3cd55563c3dd981797d59
learn <learn@learn10.local>
2016-09-29 14:17:26 -0700

    fixed navbar test

--- spec/features/nav_bars_spec.rb
+++ spec/features/nav_bars_spec.rb
@@ -11,26 +11,6 @@ RSpec.feature "NavBars", type: :feature do
         click_link 'Sign Up'
       end # ends the and
       Then 'I am taken to the Sign Up page where I can fill in my information' do
-<<<<<<< HEAD
-        fill_in 'Email', with: 'test17@me.com'
-        fill_in 'Username', with: 'username'
-        fill_in 'Password', with: 'password'
-        fill_in 'Password confirmation', with: 'password'
-      end # end of then
-      Then 'You click submit' do
-        click_button 'Sign up'
-      end # end of then
-      And 'You will be signed up and be redirected to the home page' do
-        expect(page).to have_content "Welcome! You have signed up successfully."
-        expect(page).to have_content "Welcome, username!"
-      end # end of and
-      Then 'You can click sign out and your session will end' do
-        click_link 'Sign Out'
-      end # ends then
-      And 'You will be redirected to the home page and see a flash message' do
-        expect(page).to have_content "Signed out successfully."
-        expect(page).to have_no_content("Welcome, username!")
-      end # ends and
         expect(page).to have_content "Sign up"
       end # end of and
     end # end of steps
